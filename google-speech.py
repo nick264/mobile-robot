076293932e3887aa3afff83538386cd08ee24728
Nicholas Sedlet <nicks@Nicholass-MacBook-Pro.local>
2017-03-06 23:47:46 -0500

    simple speech recognition example with google cloud speech api

---
+++ google-speech.py
@@ -0,0 +1,32 @@
+import os
+import speech_recognition as sr
+from google.cloud import speech
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'API-Project-c7afe5505e44.json'
+
+# Record Audio
+r = sr.Recognizer()
+with sr.Microphone(sample_rate = 16000) as source:
+    print("Say something!")
+    audio = r.listen(source)
+
+print("Got some audio!")
+print(audio)
+
+# Instantiates a client
+print("MAKING SPEECH CLIENT")
+speech_client = speech.Client()
+
+print("getting audio sample")
+audio_sample = speech_client.sample(
+    audio.get_raw_data(),
+    source_uri=None,
+    encoding='LINEAR16',
+    sample_rate=16000)
+
+
+# Detects speech in the audio file
+alternatives = speech_client.speech_api.sync_recognize(audio_sample)
+
+for alternative in alternatives:
+    print('Transcript: {}'.format(alternative.transcript))
